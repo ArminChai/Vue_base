f85b6ed22b285a901372310d4355541e01694fc7
arminchai <arminchai@163.com>
2022-05-22 17:58:38 +0800

    webpack开发服务器

---
+++ Day2_Code/08.webpack开发服务器/src/main.js
@@ -0,0 +1,20 @@
+import $ from 'jquery'
+import './css/index.css'
+import './less/index.less'
+import picSrc from './assets/1.gif'
+import './assets/fonts/iconfont.css'
+
+
+$('#listUl li:odd').css('color', 'orange')
+$('#listUl li:even').css('color', 'skyblue')
+
+let itemImg = document.createElement('img')
+itemImg.src = picSrc
+document.body.appendChild(itemImg)
+
+let itemI = document.createElement('i')
+itemI.className = 'iconfont icon-qq'
+document.body.appendChild(itemI)
+
+let fn = () => console.log('我是箭头函数')
+console.log(fn)
